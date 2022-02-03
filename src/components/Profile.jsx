bd9e09e368058d71d2db4261988391dbd94b85c7
unknown <gmgustavom@gmail.com>
2022-02-03 18:27:05 -0300

    creating profile component

---
+++ src/components/Profile.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Profile extends Component {
+  render() {
+    return (
+      <div data-testid="page-profile"> 🚧 PROFILE EM CONSTRUÇÂO 🚧 </div>
+    );
+  }
+}
