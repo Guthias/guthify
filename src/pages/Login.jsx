37d3d47b39372a517f087b50393182e4b808a100
unknown <gmgustavom@gmail.com>
2022-02-03 19:34:35 -0300

    Creating login HTML structure

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,22 @@ import React, { Component } from 'react';
 export default class Login extends Component {
   render() {
     return (
-      <div data-testid="page-login"> 🚧 LOGIN EM CONSTRUÇÂO 🚧 </div>
+      <div data-testid="page-login">
+        <form>
+          <label htmlFor="username-input">
+            Username
+            <input
+              type="text"
+              id="username-input"
+              data-testid="login-name-input"
+              placeholder="Nome de Usuario"
+              className="login-input"
+            />
+          </label>
+
+          <button type="submit" className="login-button">Login</button>
+        </form>
+      </div>
     );
   }
 }
