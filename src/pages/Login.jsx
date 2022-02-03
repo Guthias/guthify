e7f5f21459d9d74721725c5584ae13eb76c8cba5
unknown <gmgustavom@gmail.com>
2022-02-03 20:00:54 -0300

    style: styling Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,19 +3,16 @@ import React, { Component } from 'react';
 export default class Login extends Component {
   render() {
     return (
-      <div data-testid="page-login">
-        <form>
-          <label htmlFor="username-input">
-            Username
-            <input
-              type="text"
-              id="username-input"
-              data-testid="login-name-input"
-              placeholder="Nome de Usuario"
-              className="login-input"
-            />
-          </label>
-
+      <div data-testid="page-login" className="page-login">
+        <form className="login-form">
+          <h1 className="login-title">Guthify</h1>
+          <input
+            type="text"
+            id="username-input"
+            data-testid="login-name-input"
+            placeholder="Nome de Usuario"
+            className="login-input"
+          />
           <button type="submit" className="login-button">Login</button>
         </form>
       </div>
