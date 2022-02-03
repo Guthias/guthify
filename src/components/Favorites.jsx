03e094fbcdc9a7ae612b827890c0dc243da8e9f8
unknown <gmgustavom@gmail.com>
2022-02-03 18:26:44 -0300

    creating Favorites component

---
+++ src/components/Favorites.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Favorites extends Component {
+  render() {
+    return (
+      <div data-testid="page-favorites"> 🚧 FAVORITES EM CONSTRUÇÂO 🚧 </div>
+    );
+  }
+}
