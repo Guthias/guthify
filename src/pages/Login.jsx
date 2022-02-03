59c936be40a0e3d966a4ea732f87a87135307d80
unknown <gmgustavom@gmail.com>
2022-02-03 20:09:40 -0300

    creating loading component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
+import Loading from '../components/Loading';
 
 export default class Login extends Component {
   render() {
     return (
-      <div data-testid="page-login" className="page-login">
-        <form className="login-form">
-          <h1 className="login-title">Guthify</h1>
-          <input
-            type="text"
-            id="username-input"
-            data-testid="login-name-input"
-            placeholder="Nome de Usuario"
-            className="login-input"
-          />
-          <button type="submit" className="login-button">Login</button>
-        </form>
-      </div>
+      <>
+        <Loading />
+        <div data-testid="page-login" className="page-login">
+          <form className="login-form">
+            <h1 className="login-title">Guthify</h1>
+            <input
+              type="text"
+              id="username-input"
+              data-testid="login-name-input"
+              placeholder="Nome de Usuario"
+              className="login-input"
+            />
+            <button type="submit" className="login-button">Login</button>
+          </form>
+        </div>
+      </>
     );
   }
 }
