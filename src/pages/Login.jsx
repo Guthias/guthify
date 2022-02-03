9917eef90534b07bca7d9e5001e82d1f4f99352d
unknown <gmgustavom@gmail.com>
2022-02-03 20:29:00 -0300

    Adding events and state on Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,22 @@ import React, { Component } from 'react';
 // import Loading from '../components/Loading';
 
 export default class Login extends Component {
+  state = {
+    userNameInput: '',
+    buttonDisabled: true,
+  }
+
+  handleChange = ({ target }) => {
+    const MIN_USERNAME_LENGTH = 3;
+    this.setState({
+      userNameInput: target.value,
+      buttonDisabled: target.value.length < MIN_USERNAME_LENGTH,
+    });
+  }
+
   render() {
+    const { userNameInput, buttonDisabled } = this.state;
+
     return (
       // <Loading />
       <div data-testid="page-login" className="page-login">
@@ -10,12 +25,20 @@ export default class Login extends Component {
           <h1 className="login-title">Guthify</h1>
           <input
             type="text"
-            id="username-input"
+            id="userNameInput"
+            value={ userNameInput }
+            onChange={ this.handleChange }
             data-testid="login-name-input"
             placeholder="Nome de Usuario"
             className="login-input"
           />
-          <button type="submit" className="login-button">Login</button>
+          <button
+            type="submit"
+            className="login-button"
+            disabled={ buttonDisabled }
+          >
+            Login
+          </button>
         </form>
       </div>
     );
