1d965e3055e596b19608f8790372cc170f5e0f1c
unknown <gmgustavom@gmail.com>
2022-02-03 18:28:25 -0300

    creating ProfileEdit component

---
+++ src/components/ProfileEdit.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class ProfileEdit extends Component {
+  render() {
+    return (
+      <div data-testid="page-profile-edit"> 🚧 PROFILE EDIT EM CONSTRUÇÂO 🚧 </div>
+    );
+  }
+}
