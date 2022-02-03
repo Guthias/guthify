cf4cd494906736425009caa4ff60b34d8b229402
unknown <gmgustavom@gmail.com>
2022-02-03 18:26:24 -0300

    creating album component

---
+++ src/components/Album.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Album extends Component {
+  render() {
+    return (
+      <div data-testid="page-album"> 🚧 ALBUM EM CONSTRUÇÂO 🚧 </div>
+    );
+  }
+}
