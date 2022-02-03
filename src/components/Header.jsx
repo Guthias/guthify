7ea947c1c0c2af4611dfc8555669b86307d75708
unknown <gmgustavom@gmail.com>
2022-02-03 19:02:16 -0300

    creating header links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Header extends Component {
   render() {
     return (
       <header data-testid="header-component">
         <div className="header-title-area">Guthify</div>
-        <nav className="header-nav">Area de Links</nav>
-        <nav className="header-user">Nome do usuario</nav>
+        <nav className="header-nav">
+          <Link to="/search" data-testid="link-to-search">Search</Link>
+          <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+          <Link to="/profile" data-testid="link-to-profile">Profile</Link>
+        </nav>
+        <div className="header-user-area">Olá, usuario </div>
       </header>
     );
   }
