0b272769f9e84e21151c7894b1e96f8497f69a90
unknown <gmgustavom@gmail.com>
2022-02-03 20:20:49 -0300

    removing Content component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import Content from './components/Content';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <Content />
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route path="/search" component={ Search } />
+          <Route path="/album/:id" component={ Album } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
       </BrowserRouter>
     );
   }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import Loading from '../components/Loading';
+// import Loading from '../components/Loading';
 
 export default class Login extends Component {
   render() {
     return (
-      <>
-        <Loading />
-        <div data-testid="page-login" className="page-login">
-          <form className="login-form">
-            <h1 className="login-title">Guthify</h1>
-            <input
-              type="text"
-              id="username-input"
-              data-testid="login-name-input"
-              placeholder="Nome de Usuario"
-              className="login-input"
-            />
-            <button type="submit" className="login-button">Login</button>
-          </form>
-        </div>
-      </>
+      // <Loading />
+      <div data-testid="page-login" className="page-login">
+        <form className="login-form">
+          <h1 className="login-title">Guthify</h1>
+          <input
+            type="text"
+            id="username-input"
+            data-testid="login-name-input"
+            placeholder="Nome de Usuario"
+            className="login-input"
+          />
+          <button type="submit" className="login-button">Login</button>
+        </form>
+      </div>
     );
   }
 }
