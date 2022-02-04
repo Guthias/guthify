77d1f02ac4e32a9078eb97ec7a98b0b81c1eb071
unknown <gmgustavom@gmail.com>
2022-02-03 21:05:36 -0300

    showing loading when press button and creatingUser

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import Loading from '../components/Loading';
+import { createUser } from '../services/userAPI';
 // import Loading from '../components/Loading';
 
 export default class Login extends Component {
   state = {
     userNameInput: '',
     buttonDisabled: true,
+    loading: false,
   }
 
   handleChange = ({ target }) => {
@@ -15,32 +19,47 @@ export default class Login extends Component {
     });
   }
 
-  render() {
-    const { userNameInput, buttonDisabled } = this.state;
+  loginSubmit = async (event) => {
+    event.preventDefault();
+    const { userNameInput: name } = this.state;
+    this.setState({ loading: true });
+    await createUser(({ name }));
+    this.setState({ loading: false });
+  }
 
+  renderForm = () => {
+    const { userNameInput, buttonDisabled, loading } = this.state;
     return (
-      // <Loading />
-      <div data-testid="page-login" className="page-login">
-        <form className="login-form">
-          <h1 className="login-title">Guthify</h1>
-          <input
-            type="text"
-            id="userNameInput"
-            value={ userNameInput }
-            onChange={ this.handleChange }
-            data-testid="login-name-input"
-            placeholder="Nome de Usuario"
-            className="login-input"
-          />
-          <button
-            type="submit"
-            className="login-button"
-            disabled={ buttonDisabled }
-          >
-            Login
-          </button>
-        </form>
-      </div>
+      loading ? <Loading /> : (
+        <div data-testid="page-login" className="page-login">
+          <form className="login-form">
+            <h1 className="login-title">Guthify</h1>
+            <input
+              type="text"
+              id="userNameInput"
+              value={ userNameInput }
+              onChange={ this.handleChange }
+              data-testid="login-name-input"
+              placeholder="Nome de Usuario"
+              className="login-input"
+            />
+            <button
+              type="submit"
+              className="login-button"
+              onClick={ this.loginSubmit }
+              disabled={ buttonDisabled }
+            >
+              Login
+            </button>
+          </form>
+        </div>
+      )
+    );
+  }
+
+  render() {
+    return (
+      this.renderForm()
     );
   }
 }
