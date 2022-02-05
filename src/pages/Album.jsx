0898bd70437fb5ccb0a13ad83c5987e3adec741a
unknown <gmgustavom@gmail.com>
2022-02-05 18:50:14 -0300

    getting musics from API

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
 
 export default class Album extends Component {
+  async componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+    const musicList = await getMusics(id);
+    console.log(musicList);
+  }
+
   render() {
     return (
       <div data-testid="page-album">
         <Header />
-        🚧 ALBUM EM CONSTRUÇÂO 🚧
+        <div className="page-content" />
       </div>
     );
   }
