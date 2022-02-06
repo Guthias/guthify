47c728b937fbcb8409555ec0c171ec412e95c04c
unknown <gmgustavom@gmail.com>
2022-02-05 23:01:28 -0300

    refac: rendering Header just on valid routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Content from './components/Content';
 import Header from './components/Header';
+
+const headerRoutes = ['/search', '/album/:id', '/favorites', '/profile', '/profile/edit'];
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <Header />
+        <Route path={ headerRoutes } component={ Header } />
         <Content />
       </BrowserRouter>
     );
