661768597d13d59dba2cd1b027eafa98342bb02d
unknown <gmgustavom@gmail.com>
2022-02-09 00:53:47 -0300

    adding favorite tracks on favorites page

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import TrackList from '../components/TrackList';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
 
 export default class Favorites extends Component {
+  state = {
+    favorites: [],
+  }
+
+  async componentDidMount() {
+    this.setState({ favorites: await getFavoriteSongs() });
+  }
+
   render() {
+    const { favorites } = this.state;
     return (
-      <div data-testid="page-favorites">
-        🚧 FAVORITES EM CONSTRUÇÂO 🚧
+      <div data-testid="page-favorites" className="page-content">
+        <TrackList musicList={ favorites } />
       </div>
     );
   }
