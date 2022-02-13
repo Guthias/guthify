de08b6fb87658f1e8482fb631fa3ec929b20bacd
guthias <gmgustavom@gmail.com>
2022-02-13 17:34:35 -0300

    getting user info from API

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,35 +1,49 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { getUser } from '../services/userAPI';
 
 export default class Profile extends Component {
+  state = {
+    user: {},
+    loading: true,
+  }
+
+  async componentDidMount() {
+    this.setState({ user: await getUser(), loading: false });
+  }
+
   render() {
+    const { user, loading } = this.state;
+
     return (
-      <div data-testid="page-profile" className="page-content">
-        <form className="user-info-area">
-          <div className="user-profile-image-area">
-            <img src="user-profile-image" alt="" />
-          </div>
+      loading ? <p>Carregando...</p>
+        : (
+          <div data-testid="page-profile" className="page-content">
+            <form className="user-info-area">
+              <div className="user-profile-image-area">
+                <img src="user-profile-image" alt="" />
+              </div>
 
-          <label className="profile-label" htmlFor="profile-name">
-            Nome de Usuario
-            <input id="profile-name" className="profile-input" type="text" />
-          </label>
+              <label className="profile-label" htmlFor="profile-name">
+                Nome de Usuario
+                <input id="profile-name" className="profile-input" type="text" />
+              </label>
 
-          <label className="profile-label" htmlFor="profile-email">
-            E-mail
-            <input id="profile-email" className="profile-input" type="text" />
-          </label>
+              <label className="profile-label" htmlFor="profile-email">
+                E-mail
+                <input id="profile-email" className="profile-input" type="text" />
+              </label>
 
-          <label className="profile-label" htmlFor="profile-descritpion">
-            Descrição
-            <textarea
-              id="profile-descritpion"
-              className="profile-input profile-textarea"
-            />
-          </label>
-          <Link to="profile/edit" className="profile-button">Editar Perfil</Link>
-        </form>
-      </div>
+              <label className="profile-label" htmlFor="profile-descritpion">
+                Descrição
+                <textarea
+                  id="profile-descritpion"
+                  className="profile-input profile-textarea"
+                />
+              </label>
+              <Link to="profile/edit" className="profile-button">Editar Perfil</Link>
+            </form>
+          </div>)
     );
   }
 }
