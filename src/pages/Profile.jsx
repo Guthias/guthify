cf6ef8d448e7e09e0422924af57bec58ac6ac91a
guthias <gmgustavom@gmail.com>
2022-02-13 16:53:07 -0300

    style: styling profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,32 +4,31 @@ import { Link } from 'react-router-dom';
 export default class Profile extends Component {
   render() {
     return (
-      <div data-testid="page-profile">
-        <div className="page-content">
-          <form className="user-info-area">
-            <div className="user-profile-image-area">
-              <img src="user-profile-image" alt="" />
-            </div>
+      <div data-testid="page-profile" className="page-content">
+        <form className="user-info-area">
+          <div className="user-profile-image-area">
+            <img src="user-profile-image" alt="" />
+          </div>
 
-            <label htmlFor="profile-name">
-              Nome de Usuario
-              <input id="profile-name" type="text" />
-            </label>
+          <label className="profile-label" htmlFor="profile-name">
+            Nome de Usuario
+            <input id="profile-name" className="profile-input" type="text" />
+          </label>
 
-            <label htmlFor="profile-email">
-              E-mail
-              <input id="profile-email" type="text" />
-            </label>
+          <label className="profile-label" htmlFor="profile-email">
+            E-mail
+            <input id="profile-email" className="profile-input" type="text" />
+          </label>
 
-            <label htmlFor="profile-descritpion">
-              Descrição
-              <textarea id="profile-descritpion" />
-            </label>
-            <Link to="profile/edit">
-              <button type="button" className="profile-button">Editar Perfil</button>
-            </Link>
-          </form>
-        </div>
+          <label className="profile-label" htmlFor="profile-descritpion">
+            Descrição
+            <textarea
+              id="profile-descritpion"
+              className="profile-input profile-textarea"
+            />
+          </label>
+          <Link to="profile/edit" className="profile-button">Editar Perfil</Link>
+        </form>
       </div>
     );
   }
