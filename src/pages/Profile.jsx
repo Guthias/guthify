588d8b8efe498e9b1eb456aed395a5e8c4b41104
guthias <gmgustavom@gmail.com>
2022-02-13 17:48:17 -0300

    rendering user info on profile Page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -21,27 +21,41 @@ export default class Profile extends Component {
           <div data-testid="page-profile" className="page-content">
             <form className="user-info-area">
               <div className="user-profile-image-area">
-                <img src="user-profile-image" alt="" />
+                <img data-testid="profile-image" src={ user.image } alt="" />
               </div>
 
               <label className="profile-label" htmlFor="profile-name">
                 Nome de Usuario
-                <input id="profile-name" className="profile-input" type="text" />
+                <input
+                  id="profile-name"
+                  value={ user.name }
+                  className="profile-input"
+                  type="text"
+                  disabled
+                />
               </label>
 
               <label className="profile-label" htmlFor="profile-email">
                 E-mail
-                <input id="profile-email" className="profile-input" type="text" />
+                <input
+                  id="profile-email"
+                  className="profile-input"
+                  type="text"
+                  value={ user.email }
+                  disabled
+                />
               </label>
 
               <label className="profile-label" htmlFor="profile-descritpion">
                 Descrição
                 <textarea
                   id="profile-descritpion"
+                  value={ user.description }
+                  disabled
                   className="profile-input profile-textarea"
                 />
               </label>
-              <Link to="profile/edit" className="profile-button">Editar Perfil</Link>
+              <Link to="profile/edit" className="profile-button">Editar perfil</Link>
             </form>
           </div>)
     );
