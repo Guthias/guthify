0470114096b818910777afa1a665240198ba918d
guthias <gmgustavom@gmail.com>
2022-02-13 17:53:23 -0300

    creating profile edit structure

--- src/pages/ProfileEdit.jsx
+++ src/pages/ProfileEdit.jsx
@@ -2,10 +2,44 @@ import React, { Component } from 'react';
 
 export default class ProfileEdit extends Component {
   render() {
+    const loading = false;
     return (
-      <div data-testid="page-profile-edit">
-        🚧 PROFILE EDIT EM CONSTRUÇÂO 🚧
-      </div>
+      loading ? <p>Carregando...</p>
+        : (
+          <div data-testid="page-profile" className="page-content">
+            <form className="user-info-area">
+              <div className="user-profile-image-area">
+                <img data-testid="profile-image" alt="" />
+              </div>
+
+              <label className="profile-label" htmlFor="profile-name">
+                Nome de Usuario
+                <input
+                  id="profile-name"
+                  className="profile-input"
+                  type="text"
+                />
+              </label>
+
+              <label className="profile-label" htmlFor="profile-email">
+                E-mail
+                <input
+                  id="profile-email"
+                  className="profile-input"
+                  type="text"
+                />
+              </label>
+
+              <label className="profile-label" htmlFor="profile-descritpion">
+                Descrição
+                <textarea
+                  id="profile-descritpion"
+                  className="profile-input profile-textarea"
+                />
+              </label>
+              <button type="submit" className="profile-button">Salvar</button>
+            </form>
+          </div>)
     );
   }
 }
