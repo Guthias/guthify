1121c5d0c525d2f771ef3e51b4c4f2b596c91bf4
Gustavo Mathias <gmgustavom@gmail.com>
2022-06-12 23:26:24 -0300

    style: adding logo and icons on aside

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+
+import { FaSearch, FaPlayCircle } from 'react-icons/fa';
+import { IoPerson } from 'react-icons/io5';
+
+import Logo from '../images/logo-guthify.png';
 
 const AsideArea = styled.aside`
   display: flex;
@@ -12,10 +17,6 @@ const AsideArea = styled.aside`
   nav {
     display: flex;
     flex-direction: column;
-    
-    h1 {
-      text-align: center;
-    }
 
     a {
       color: #7A7A79;
@@ -24,7 +25,7 @@ const AsideArea = styled.aside`
       padding: 0.5em 1.5em;
       font-weight: bold;
       border-left: 8px solid transparent;
-
+      border-image-slice: 1;
       :hover {
         background-color: #303030;
       }
@@ -32,19 +33,50 @@ const AsideArea = styled.aside`
       &.active {
         color: white;
         border-color: #147fdd;
+        border-image-source: linear-gradient(0deg, #147fdd, #08b2d4);
+      }
+
+      svg {
+        margin-right: 0.5em;
       }
     }
+  }
+`;
+
+const LogoArea = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 2em 0;
+
+  h1 {
+    display: none;
+  }
+
+  img {
+    width: 80%;
   }
 `;
 
 export default function Aside() {
   return (
     <AsideArea>
-      <h1>Guthify</h1>
+      <LogoArea>
+        <img src={ Logo } alt="Logo do Guthify" />
+        <h1>Guthify</h1>
+      </LogoArea>
       <nav>
-        <NavLink to="/search">Search</NavLink>
-        <NavLink to="/favorites">Favorites</NavLink>
-        <NavLink to="/profile">Profile</NavLink>
+        <NavLink to="/search">
+          <FaSearch />
+          Search
+        </NavLink>
+        <NavLink to="/favorites">
+          <FaPlayCircle />
+          Favorites
+        </NavLink>
+        <NavLink to="/profile">
+          <IoPerson />
+          Profile
+        </NavLink>
       </nav>
     </AsideArea>
   );
